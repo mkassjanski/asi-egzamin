061c9f2fd74da1f60cb1d5b78eb47b9644194508
mkassjanski <ankoupl80@gmail.com>
2017-05-13 17:15:15 +0200

    usuwanie komentarzy

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,6 +5,13 @@ class CommentsController < ApplicationController
   redirect_to film_path(@film)
 end
 
+def destroy
+  @film = Film.find(params[:film_id])
+  @comment = @film.comments.find(params[:id])
+  @comment.destroy
+  redirect_to film_path(@film)
+end
+
 private
   def comment_params
     params.require(:comment).permit(:commenter, :body)
