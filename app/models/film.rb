17734d225ceb3f1c097fa69fef2ac2ff478a3e5c
mkassjanski <ankoupl@o2.pl>
2017-05-14 16:11:59 +0200

    https

--- app/models/film.rb
+++ app/models/film.rb
@@ -1,7 +1,7 @@
 class Film < ApplicationRecord
   belongs_to :user
 	has_many :comments, dependent: :destroy
-  has_attached_file :poster, default_url: "https://www.thestage.com/images/NoImageAvailable.png"
+  has_attached_file :poster, default_url: "https://www.thestage.com/images/NoImageAvailable.png", s3_protocol => :https
   validates_attachment_content_type :poster, content_type: /\Aimage\/.*\z/
   validates :title, presence: true,
                   length: { minimum: 5 }
